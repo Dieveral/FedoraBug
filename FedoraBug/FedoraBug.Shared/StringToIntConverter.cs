121cb8511b4312924758cdc70009d02a2accbb54
Dieveral <dieveral@gmail.com>
2022-02-24 14:53:44 +0300

    Add project files.

---
+++ FedoraBug/FedoraBug.Shared/StringToIntConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FedoraBug
+{
+    public class StringToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string stringValue && int.TryParse(stringValue, out int intValue))
+            {
+                return new GridLength(intValue);
+            }
+            else
+            {
+                return new GridLength(0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FedoraBug/FedoraBug.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace FedoraBug.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new FedoraBug.App(), args);
+            host.Run();
+        }
+    }
+}
